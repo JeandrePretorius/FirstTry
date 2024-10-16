50e49cba9a50efbb3ad27ff893b6e737cc2d2d08
Jeandre Pretorius <jeandre.pretorius@prodigyfinance.com>
2024-10-16 10:27:26 +0200

    First API made available to Front End

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,17 @@ module FirstTry
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    # Enable CORS
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3001' # Your React app's origin
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
