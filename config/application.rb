4806ac3251328af0cb84fc4f3490a22a5dbb0690
Jeandre Pretorius <jeandre.pretorius@prodigyfinance.com>
2024-10-17 09:15:30 +0200

    Added seeders and CRUD functions

--- config/application.rb
+++ config/application.rb
@@ -19,11 +19,11 @@ module FirstTry
     # Enable CORS
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3001' # Your React app's origin
+        origins '*' # Your React app's origin
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
-          credentials: true
+          credentials: false
       end
     end
 
